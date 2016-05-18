52886415d924c8f91ea30e4ae1b68f8ed39337ac
Sirikhun Triwittayakhun <ty@coins.co.th>
2016-05-18 14:57:15 +0700

    add details to signup form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,4 +12,11 @@ class UsersController < ApplicationController
     end
   end
 
+  def sign_up_params
+    params.require(:user).permit(:name, :address, :postal_code, :country, :contact_number, :email, :password, :password_confirmation)
+  end
+
+  def account_update_params
+    params.require(:user).permit(:name, :address, :postal_code, :country, :contact_number, :email, :password, :password_confirmation, :current_password)
+  end
 end
