73312572511b7aef1d4137802ec443c2d39c46b9
Sirikhun Triwittayakhun <ty@coins.co.th>
2016-06-10 14:26:28 +0700

    fix conflicts

--- config/initializers/devise_permitted_parameters.rb
+++ config/initializers/devise_permitted_parameters.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 module DevisePermittedParameters
   extend ActiveSupport::Concern
 
@@ -16,22 +15,3 @@ module DevisePermittedParameters
 end
 
 DeviseController.send :include, DevisePermittedParameters
-=======
-module DevisePermittedParameters
-  extend ActiveSupport::Concern
-
-  included do
-    before_action :configure_permitted_parameters
-  end
-
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password, :address, :postal_code, :country, :contact_number) }
-    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :password, :current_password, :address, :postal_code, :country, :contact_number) }
-  end
-
-end
-
-DeviseController.send :include, DevisePermittedParameters
->>>>>>> e1546abc0e72d495d69c06e1cb37d009f2b3ec39
