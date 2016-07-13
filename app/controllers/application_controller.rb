c3bdb1228c065b677e6b4a88242adafa66001bcd
Sirikhun Triwittayakhun <ty@coins.co.th>
2016-07-13 22:55:30 +0700

    fixed Devise gem .for to .permit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   protected
 
     def configure_permitted_parameters
-        devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password, :address, :postal_code, :country, :contact_number) }
-        devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :password, :current_password, :address, :postal_code, :country, :contact_number) }
+        devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :address, :postal_code, :country, :contact_number) }
+        devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password, :address, :postal_code, :country, :contact_number) }
     end
 end
