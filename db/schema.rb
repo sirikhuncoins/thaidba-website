7828c9a8d0d55e9fde18b20eb4cab3d1cae16935
garywong <gary@coins.co.th>
2016-07-28 14:19:48 +0700

    add new approval field

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160603092538) do
+ActiveRecord::Schema.define(version: 20160728070716) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "comment"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 20160603092538) do
     t.string   "postal_code"
     t.string   "country"
     t.string   "contact_number"
+    t.boolean  "approved"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
